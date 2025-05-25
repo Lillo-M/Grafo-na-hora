49c09449283b81319f827cc1c4e21bb39ede85e6
Omar <omarzagonel@gmail.com>
2025-05-25 20:54:39 -0300

    initial creation of models and insertion of data on the db

--- Backend/grafoNaHoraAPI/grafoNaHora/models.py
+++ Backend/grafoNaHoraAPI/grafoNaHora/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Disciplina(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    nome   = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.id}'
+
+class Curso(models.Model):
+    # id auto
+    nome    = models.CharField(max_length=200)
+    duracao = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.id}'
+
+class Matriz(models.Model):
+    id     = models.PositiveSmallIntegerField(primary_key=True)
+    curso  = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    versao = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.id}'
+
+class Optativa(models.Model): # trilha
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.id}'
+
+class OpcaoDisciplinas(models.Model):
+    # id auto
+    matriz        = models.ForeignKey(Matriz, on_delete=models.CASCADE)
+    periodo       = models.PositiveSmallIntegerField()
+    optativa      = models.ForeignKey(Optativa, on_delete=models.CASCADE) # trilha
+    disciplina    = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    carga_horaria = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+       return f'{self.id}'
