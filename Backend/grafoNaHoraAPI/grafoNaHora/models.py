7906193c5cf76ed3e8d02ce07c207604209672bb
Jean Cunha <majornix051@gmail.com>
2025-06-07 19:44:42 -0300

    Adição dos imports nas models

--- Backend/grafoNaHoraAPI/grafoNaHora/models.py
+++ Backend/grafoNaHoraAPI/grafoNaHora/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 class Disciplina(models.Model):
     id   = models.CharField(max_length=20, primary_key=True) #codigo
